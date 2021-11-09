eb2e2bd12125f1b5e7d087ad6ddd394e397eb426
robodmanx <43498235+robodmanx@users.noreply.github.com>
2021-11-08 19:25:42 -0500

    Update main.py

--- BoBot/main.py
+++ BoBot/main.py
@@ -5,6 +5,10 @@ from discord.ext import commands
 import os
 import json
 
+# setup
+
+os.chdir(./BoBot)
+
 # config
 
 def accessConfig(server: int = 890220889688408074, param: str = '', mode: str = 'read', to_write = None): 
@@ -45,7 +49,7 @@ async def sendEmbed(ctx, message = '', title = '', img = '', attach = None, foot
 
 # cogs
 
-for file in os.listdir('./BoBot/cogs'):
+for file in os.listdir('./cogs'):
   if file.endswith('.py'):
     client.load_extension(f'cogs.{file[:-3]}')
 
